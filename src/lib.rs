910abd7c8c11c471c829292a5b774f683f032b03
SpiralP <spiral.palmer@gmail.com>
2019-11-02 21:31:39 -0700

    structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,68 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
+use error_chain::error_chain;
+use futures::{channel::mpsc::*, prelude::*};
+use serde::Serialize;
+use std::pin::Pin;
+
+error_chain! {
+  types {
+    Error, ErrorKind, ResultExt, Result;
+  }
+}
+
+pub struct NetworkGraph<T> {
+  sink: Pin<Box<T>>,
+  receiver: UnboundedReceiver<Vec<u8>>,
+}
+
+impl<T> NetworkGraph<T>
+where
+  T: Sink<Vec<u8>>,
+  T::Error: std::error::Error + Send + 'static,
+{
+  pub fn new(sink: T) -> (Self, NetworkGraphController) {
+    let (sender, receiver) = unbounded();
+
+    (
+      Self {
+        sink: Box::pin(sink),
+        receiver,
+      },
+      NetworkGraphController { sender },
+    )
+  }
+
+  pub async fn process(mut self) -> Result<()> {
+    while let Some(bytes) = self.receiver.next().await {
+      self
+        .sink
+        .send(bytes)
+        .await
+        .chain_err(|| "sink send error")?;
+    }
+
+    Ok(())
+  }
+}
+
+#[derive(Clone)]
+pub struct NetworkGraphController {
+  sender: UnboundedSender<Vec<u8>>,
+}
+
+impl NetworkGraphController {
+  pub async fn add_node(&mut self) -> Result<()> {
+    #[derive(Serialize)]
+    struct Node {};
+
+    let node = Node {};
+    let serialized = serde_json::to_vec(&node).chain_err(|| "serde_json serialize error")?;
+
+    self
+      .sender
+      .send(serialized)
+      .await
+      .chain_err(|| "sink send error")?;
+
+    Ok(())
   }
 }
